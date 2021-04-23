0acf33b60a55da3cf667f773d1ca6b43101cc5ab
negiseijin <satoru6221@gmail.com>
2021-04-23 14:45:58 +0900

    :recycle: PDFの文字化け修正

--- pdf-configs/config.js
+++ pdf-configs/config.js
@@ -2,6 +2,7 @@ module.exports = {
   stylesheet:
     'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css',
   css: `
+  body { font-family: "Hiragino Kaku Gothic ProN", Meiryo, sans-serif; }
   .page-break { page-break-after: always; }
   .markdown-body { font-size: 11px; }
   .markdown-body pre > code { white-space: pre-wrap; }
